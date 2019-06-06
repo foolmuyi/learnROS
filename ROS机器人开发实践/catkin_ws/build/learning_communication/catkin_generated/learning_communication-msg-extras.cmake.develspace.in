e8e429cba2a3bc35a77d5eb19435ebea22e977e6
muyi <muyiorlk@gmail.com>
2019-06-06 21:40:41 +0800

    delete everything

--- ROS机器人开发实践/catkin_ws/build/learning_communication/catkin_generated/learning_communication-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(learning_communication_MESSAGE_FILES "/home/muyi/catkin_ws/src/learning_communication/msg/Person.msg")
-set(learning_communication_SERVICE_FILES "")
--- ROS机器人开发实践/catkin_ws/devel/share/common-lisp/ros/learning_communication/msg/_package_Person.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package learning_communication-msg)
-(cl:export '(NAME-VAL
-          NAME
-          GENDER-VAL
-          GENDER
-          AGE-VAL
-          AGE
-))
